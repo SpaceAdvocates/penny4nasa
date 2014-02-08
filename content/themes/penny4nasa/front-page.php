c09a3159ab00f90e88319f900bd9827929411418
Joseph Spens <joseph@workmarket.com>
2014-02-08 15:57:23 -0500

    Added the moon

--- content/themes/penny4nasa/front-page.php
+++ content/themes/penny4nasa/front-page.php
@@ -1,7 +1,48 @@
 <?php get_header(); ?>
 
-<div class="container">
-	<div class="hero-video"></div>
+<div class="hero-video">
+	<div class="container">
+		<div class="video"></div>
+	</div>
 </div>
+
+<div class="hero-cta">
+	<div class="container">
+		<div class="content">
+			<h1 class="hero-cta--header">Join our Mission</h1>
+			<p class="hero-cta--subheader">To increase NASA's funding to 1% by encouraging popular support for NASA through education and outreach.</p>
+			<a href="javascript:void(0)" class="hero-cta--link">Take Action</a>
+			<ul class="social-links">
+				<li class="social-link facebook">
+					<a href="javascript:void(0)">
+						<i></i>
+					</a>
+				</li>
+				<li class="social-link twitter">
+					<a href="javascript:void(0)">
+						<i></i>
+					</a>
+				</li>
+				<li class="social-link google-plus">
+					<a href="javascript:void(0)">
+						<i></i>
+					</a>
+				</li>
+				<li class="social-link youtube">
+					<a href="javascript:void(0)">
+						<i></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="hero-banner">
+	<div class="content">
+		<div class="container">
+
+		</div>
+	</div>
 
 <?php get_footer(); ?>
