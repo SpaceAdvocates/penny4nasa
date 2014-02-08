2a78fade547b8539d1a7403a89f7cd06b89b8462
Joseph Spens <joseph@workmarket.com>
2014-02-08 18:30:39 -0500

    Made the homepage social links inline with cta button

---
+++ content/themes/penny4nasa/blog.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+	<?php
+		if ( have_posts() ) :
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the post format-specific template for the content. If you want to
+				 * use this in a child theme, then include a file called called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+
+			endwhile;
+
+		else :
+			// If no content, include the "No posts found" template.
+			get_template_part( 'content', 'none' );
+
+		endif;
+	?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
