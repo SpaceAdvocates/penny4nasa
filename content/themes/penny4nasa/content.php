08abd3e5ba522e16bb73f76ec9d40c03a1d3f571
Joseph Spens <joseph@workmarket.com>
2014-02-08 23:16:45 -0500

    Styled blog post titles

--- content/themes/penny4nasa/content.php
+++ content/themes/penny4nasa/content.php
@@ -1,21 +1,9 @@
-<?php
-/**
- * The default template for displaying content
- *
- * Used for both single and index/archive/search.
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
-?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php twentyfourteen_post_thumbnail(); ?>
 
 	<header class="entry-header">
 		<h1 class="entry-title">
-			<a href="<?php esc_url( get_permalink() ) ?>" rel="bookmark">
+			<a href="<?php echo esc_url(get_permalink()); ?>" rel="bookmark">
 				<?php the_title(); ?>
 			</a>
 		</h1>
