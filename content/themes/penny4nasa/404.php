96108ba08751424bb581c086a63218763d75f85f
Joseph Spens <joseph@workmarket.com>
2014-03-07 17:57:05 -0500

    Resolve #5 by adding styling for 404 page

--- content/themes/penny4nasa/404.php
+++ content/themes/penny4nasa/404.php
@@ -9,24 +9,16 @@
 
 get_header(); ?>
 
+	<div class="container">
+		<header class="page-header">
+			<h1 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h1>
+		</header>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h1>
-			</header>
-
-			<div class="page-content">
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfourteen' ); ?></p>
-
-				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
-
-		</div><!-- #content -->
+		<div class="page-content">
+			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfourteen' ); ?></p>
+			<?php get_search_form(); ?>
+		</div><!-- .page-content -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar( 'content' );
-get_sidebar();
 get_footer();
