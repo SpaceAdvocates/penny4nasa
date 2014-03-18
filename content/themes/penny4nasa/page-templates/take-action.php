6230859c178b774248b65bbe5e98b87d9378758a
Joseph Spens <joseph@workmarket.com>
2014-03-18 18:51:56 -0400

    Add Take Action page

---
+++ content/themes/penny4nasa/page-templates/take-action.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Take Action Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div class="container">
+	<?php get_sidebar(); ?>
+	<div class="content">
+		<h1 class="page-header">
+			<?php the_title(); ?>
+		</h1>
+		<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+				// Include the page content template.
+				get_template_part( 'content', 'notice' );
+
+			endwhile;
+		?>
+	</div>
+<?php
+get_footer();
